6d492353da69d28f3724a47e84bc178ce4caf34c
Sebastian-Giraldo <giraldo.sebastian@correounivalle.edu.co>
2025-05-21 21:33:54 -0500

    update page consulta

--- package-lock/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-// ProtectedRoute.jsx
-import { Navigate, Outlet } from "react-router-dom";
-import { useUser }          from "../context/UserContext";
-
-export default function ProtectedRoute() {
-  const { user } = useUser();
-  return user ? <Outlet /> : <Navigate to="/" replace />;
-}
